3b63c18ef732ecc7e2a2bbe41c9762d48c745d3f
YouGee <yougee@gmail.com>
2013-08-24 09:29:28 +0200

    WIP: SHARD engine 0.0.2
    - Dynamic debug line
    - Image ressources structure
    - Zoom OK
    - Panning OK
    - Sublime sword cursor
    - Display acceptable destination tiles

--- main.lua
+++ main.lua
@@ -26,9 +26,21 @@ function love.load()
     success = love.graphics.setMode(xdisplaySize,ydisplaySize,false,true,0)
 
     -- Loading graphical ressources
-    player_IMG            = love.graphics.newImage("Media/Graphic/Sprites/S_Heroe_Warrior.png")
-    tile_black_IMG        = love.graphics.newImage("Media/Graphic/Tiles/tile_black.png")
-    panel_IMG             = love.graphics.newImage("Media/Graphic/Panel/Panel.png")
+    imageRessource = {}
+        -- player
+    imageRessource.player   = love.graphics.newImage("Media/Graphic/Sprites/S_Heroe_Warrior.png")
+        -- panel
+    imageRessource.panel    = love.graphics.newImage("Media/Graphic/Panel/Panel.png")
+        -- tiles
+    imageRessource.tile     = {}
+    imageRessource.tile.UKN = love.graphics.newImage("Media/Graphic/Tiles/tile_black.png")
+        -- destination icon
+    imageRessource.dest     = love.graphics.newImage("Media/Graphic/Sprites/Shoeprint.png")
+
+    -- mouse cursor
+    cursorImg = love.graphics.newImage("Media/Graphic/Mouse/Sword.png") -- load in a custom mouse image
+    love.mouse.setVisible(false)
+    love.mouse.setGrab(false)
 
     -- Background sound loading
     if PlaySound == "yes" then
@@ -69,9 +81,10 @@ function love.load()
     tile.CurrentSize = tile.NativeSize*zoomLevel.current
 
     -- On screen debug data
-    debugRow        = xdisplaySize-180
-    debugFirstLine  = panel_IMG:getHeight()- 100
-    debugDeltaLines = 20
+    debugRow         = xdisplaySize-180
+    debugFirstLine   = imageRessource.panel:getHeight()- 200
+    debugDeltaLine   = 20
+    debugCurrentLine = debugFirstLine
 
     -- Default color
     defaultColor = {}
@@ -83,19 +96,32 @@ function love.load()
 
     -- Zoom dependant displayed tiled area
     displayableTiles = {}
-    anchorTile = {} -- first bottom left displayed cell
-    anchorTile.x = 1
-    anchorTile.y = 1
+    minTile = {} -- first bottom left displayed cell
+    minTile.x = 1
+    minTile.y = 1
     maxTile = {} -- last top right displayed cell
+    maxTile.x = nil
+    maxTile.y = nil
 
+    -- Tiles associated graphics
+    terrainImage = {}
+    terrainImage["UKN"] = tile_black_IMG
 
     -- Tiled world definition
     world = {}
-    for lin=1,chosenWorldSize.x do
-        world[lin] = {}
-        for row=1,chosenWorldSize.y do
-            world[lin][row] = {}
-            world[lin][row]["graphic"] = tile_black_IMG -- unexplored tile
+    for x=1,chosenWorldSize.x do
+        world[x] = {}
+        for y=1,chosenWorldSize.y do
+            world[x][y] = {}
+            world[x][y]["type"] = "UKN" -- unexplored tile
+            world[x][y]["visible"] = "no"
+            world[x][y]["orient"] = 0
+            world[x][y]["display"] = {}
+            world[x][y]["display"]["x"] = x
+            world[x][y]["display"]["y"] = y
+            world[x][y]["valid_dest"] = "no"
+            world[x][y]["player"] = "no"
+            world[x][y]["actor"] = nil
         end
     end
 
@@ -103,10 +129,72 @@ function love.load()
     hero = {}
     hero.x,hero.y = 4,3 -- x,y cell
     hero.orient = 0 -- up
+    world[4][3]["player"] = "yes"
+
+    -- Mouse
+    mouse = {}
+    mouseIsInTile = {}
+
+    -- world panning
+    shifting = {}
+    shifting.x,shifting.y = 0,0
 end
 
 function love.update(dt)
         -- called before a frame is drawn, all math should be done in here
+
+    -- reset debugCurrentLine
+    debugCurrentLine = debugFirstLine
+
+    -- how many tiles can be displayed
+    displayableTiles.x,displayableTiles.y = maxDisplayableTiles()
+    
+    -- set the bottom left tile
+    if (minTile.x+displayableTiles.x-1) > chosenWorldSize.x then
+        minTile.x = 1 -- minTile.x too big
+    end
+    if (minTile.y+displayableTiles.y-1) > chosenWorldSize.y then
+        minTile.y = 1 -- minTile.y too big
+    end
+   
+    -- set the upper right tile
+    maxTile.x = minTile.x + displayableTiles.x - 1
+    maxTile.y = minTile.y + displayableTiles.y - 1
+
+    -- update mouse position
+    mouse.x, mouse.y = love.mouse.getPosition()
+    mouseIsInTile.x,mouseIsInTile.y = physical2tile(mouse.x, mouse.y)
+    
+    -- update world tiles
+    for x=1,chosenWorldSize.x do
+        for y=1,chosenWorldSize.y do
+            -- visibility
+            if  (x >= minTile.x) and
+                (x <= maxTile.x) and
+                (y >= minTile.y) and
+                (y <= maxTile.y) then
+                world[x][y]["visible"] = "yes"
+            else
+                world[x][y]["visible"] = "no"
+            end
+            -- shifting
+            if shifting.x ~= 0 then
+                world[x][y]["display"]["x"] =  world[x][y]["display"]["x"] + shifting.x
+            end
+            if shifting.y ~= 0 then
+                world[x][y]["display"]["y"] =  world[x][y]["display"]["y"] + shifting.y
+            end
+            -- valid destination tile?
+            if (mouseIsInTile.x == x) and (mouseIsInTile.y == y) and playerIsNear(x,y) then
+                world[x][y]["valid_dest"] = "yes"
+            else
+                world[x][y]["valid_dest"] = "no"
+            end
+        end
+    end
+
+    -- reset panning
+    shifting.x,shifting.y = 0,0
 
 end
 
@@ -114,44 +202,45 @@ function love.draw()
         -- draws all stuff to the screen
 
     -- display right panel
-    love.graphics.draw(panel_IMG,xdisplaySize-panel_IMG:getWidth(),0)
+    love.graphics.draw(imageRessource.panel,xdisplaySize-imageRessource.panel:getWidth(),0)
+    printDebugLine("dispTiles: "..displayableTiles.x..","..displayableTiles.y, "black")
+    printDebugLine("minTile: "..minTile.x..","..minTile.y, "black")
+    printDebugLine("maxTile: "..maxTile.x..","..maxTile.y, "black")
 
-    -- update anchorTile (zoom can change it)
-    displayableTiles.x,displayableTiles.y = maxDisplayableTiles()
-    if (anchorTile.x+displayableTiles.x-1) > chosenWorldSize.x then
-        anchorTile.x = 1
-    end
-    if (anchorTile.y+displayableTiles.y-1) > chosenWorldSize.y then
-        anchorTile.y = 1
-    end
-
-    -- draw tiles
-    print("------ New tiles refresh, zoom ="..zoomLevel.current.." ------")
-    print("MaxTiles "..displayableTiles.x..","..displayableTiles.y)
-    for displayedX=1,displayableTiles.x do
-        for displayedY=1,displayableTiles.y do
-            tileIDx = anchorTile.x + displayedX -1
-            tileIDy = anchorTile.y + displayedY -1
-            print("tileIDx = "..tileIDx..", tileIDy ="..tileIDy)
-            drawTile(world[tileIDx][tileIDy]["graphic"],displayedX,displayedY)
-            -- debug
-            local upperLeftX,upperLeftY = logical2physical(displayedX,displayedY,tile.NativeSize,tile.NativeSize)
-            love.graphics.print(tileIDx..","..tileIDy,upperLeftX+(tile.NativeSize*zoomLevel.current/4),upperLeftY+(tile.NativeSize*zoomLevel.current/3))
+    -- display tiles
+    for x=1,chosenWorldSize.x do
+        for y=1,chosenWorldSize.y do
+            if world[x][y]["visible"] == "yes" then
+                -- display the tile
+                drawTile(imageRessource.tile[world[x][y]["type"]],world[x][y]["display"]["x"],world[x][y]["display"]["y"])
+                -- debug: display cell coordinates
+                local upperLeftX,upperLeftY = logical2physical(world[x][y]["display"]["x"],world[x][y]["display"]["y"],tile.NativeSize,tile.NativeSize)
+                love.graphics.print(x..","..y,upperLeftX+(tile.NativeSize*zoomLevel.current/4),upperLeftY+(tile.NativeSize*zoomLevel.current/3))
+                -- display this cell as a valid destination?
+                if world[x][y]["valid_dest"] == "yes" then
+                    local offsetX,offsetY = imageOffset(imageRessource.dest)
+                    local x,y = logical2physical(world[x][y]["display"]["x"],world[x][y]["display"]["y"],tile.NativeSize,tile.NativeSize)
+                    love.graphics.draw(imageRessource.dest,x,y,0,zoomLevel.current,zoomLevel.current,offsetX,offsetY)
+                    printDebugLine("Feet offset "..offsetX..","..offsetY, "black")
+                end
+                -- display the player?
+                if world[x][y]["player"] == "yes" then
+                    local offsetX,offsetY = imageOffset(imageRessource.player)
+                    local x,y = logical2physical(world[x][y]["display"]["x"],world[x][y]["display"]["y"],tile.NativeSize,tile.NativeSize)
+                    love.graphics.draw(imageRessource.player,x,y,hero.orient,zoomLevel.current,zoomLevel.current,offsetX,offsetY)
+                    printDebugLine("Player is at "..x..","..y, "black")
+                end
+            end
         end
     end
-    -- remember the upper right displayed tile
-    maxTile.x = anchorTile.x + displayableTiles.x - 1
-    maxTile.y = anchorTile.y + displayableTiles.y - 1
 
-    -- Draw player at current position
-    local offsetX,offsetY = imageOffset(player_IMG)
-    local x,y = logical2physical(hero.x+anchorTile.x-1,hero.y+anchorTile.y-1,tile.NativeSize,tile.NativeSize)
-    love.graphics.draw(player_IMG,x,y,0,zoomLevel.current,zoomLevel.current,offsetX,offsetY)
-    printDebugLine("Player is at "..x..","..y, 2, "black")
+    -- Draw the custom mouse cursor
+    love.graphics.draw(cursorImg, mouse.x, mouse.y)
+    printDebugLine("Mouse in "..mouseIsInTile.x..","..mouseIsInTile.y, "black")
 
     -- Draw debug informations
-    printDebugLine("Mouse is at "..love.mouse.getX()..","..love.mouse.getY(), 1, "black")
-    printDebugLine("Zoom Level is "..zoomLevel.current, 3, "black")
+    printDebugLine("Mouse is at "..love.mouse.getX()..","..love.mouse.getY(), "black")
+    printDebugLine("Zoom Level is "..zoomLevel.current, "black")
 end
 
 function drawTile(ressource,logicalX,logicalY)
@@ -164,9 +253,19 @@ function logical2physical(logicalX,logicalY,imageSizeX,imageSizeY)
     return ((logicalX-1)*imageSizeX*zoomLevel.current),(ydisplaySize - (logicalY*imageSizeY*zoomLevel.current))
 end
 
+function physical2tile(physicalX,physicalY)
+    -- tells wether physicalX,physicalY point is within a tile or not
+    local tileX,tileY = math.floor(physicalX/tile.CurrentSize)+1,math.floor((ydisplaySize-physicalY)/tile.CurrentSize)+1
+    if (tileX > displayableTiles.x) or (tileY > displayableTiles.y) then
+        tileX = "out"
+        tileY = "out"
+    end
+    return tileX,tileY
+end
+
 function imageOffset(ressource)
-    local ressourceWidth =  ressource:getWidth()*zoomLevel.current
-    local ressourceHeight = ressource:getHeight()*zoomLevel.current
+    -- computes the offset for displaying an image in the center of a tile
+    local ressourceWidth,ressourceHeight =  ressource:getWidth()*zoomLevel.current,ressource:getHeight()*zoomLevel.current
     return -(tile.CurrentSize-ressourceWidth)/2,-(tile.CurrentSize-ressourceHeight)/2
 end
 
@@ -177,18 +276,24 @@ function love.keyreleased(key)
     end
     -- map panning
     if key == "left" then
-        anchorTile.x = math.max(anchorTile.x-1,1)
+        if minTile.x > 1 then
+            shifting.x,shifting.y = 1,0
+            minTile.x = minTile.x-1
+        end
     elseif key == "right" then
-        -- cant we pan?
         if maxTile.x < chosenWorldSize.x then
-            anchorTile.x = math.min(anchorTile.x+1,chosenWorldSize.x)
+            shifting.x,shifting.y = -1,0
+            minTile.x = minTile.x+1
         end
     elseif key == "down" then
-        anchorTile.y = math.max(anchorTile.y-1,1)
+        if minTile.y > 1 then
+            shifting.x,shifting.y = 0,1
+            minTile.y = minTile.y-1
+        end
     elseif key == "up" then
-        -- cant we pan?
         if maxTile.y < chosenWorldSize.y then
-            anchorTile.y = math.min(anchorTile.y+1,chosenWorldSize.y)
+            shifting.x,shifting.y = 0,-1
+            minTile.y = minTile.y+1
         end
     end
 end
@@ -209,10 +314,11 @@ function love.mousepressed(x,y,button)
     tile.CurrentSize = tile.NativeSize*zoomLevel.current
 end
 
-function printDebugLine(data,line,aColor)
+function printDebugLine(data,aColor)
     -- print debug line in top right corner of the graphical window
     mySetColor(aColor)
-    love.graphics.print(data, debugRow, debugFirstLine+(debugDeltaLines*(line-1)))
+    love.graphics.print(data, debugRow, debugCurrentLine)
+    debugCurrentLine = debugCurrentLine+debugDeltaLine
     mySetColor("default")
 end
 
@@ -229,14 +335,24 @@ end
 function maxDisplayableTiles()
     -- returns the number of tiles that can be displayed given the window size and the current zoom
     local verticalEmptySpace = 5 -- pixels left blank between right most tiles and the panel
-    local xTiles = (xdisplaySize-(panel_IMG:getWidth()+verticalEmptySpace))/tile.CurrentSize
+    local xTiles = (xdisplaySize-(imageRessource.panel:getWidth()+verticalEmptySpace))/tile.CurrentSize
     local yTiles = ydisplaySize/tile.CurrentSize
     return math.min(chosenWorldSize.x,math.floor(xTiles)),math.min(chosenWorldSize.y,math.floor(yTiles))
 end
 
+function playerIsNear(x,y)
+    -- is the tile x,y adjacent to the player?
+    if (x == hero.x) and ((y == hero.y-1) or (y == hero.y+1)) then
+        return true
+    elseif ((x == hero.x+1) or (x == hero.x-1)) and (y == hero.y) then
+        return true
+    else
+        return false
+    end
+end
+
 -- **BUGS**
---  * Joueur mal positionné si zoom/panning
---  * Gérer coorectement coordonnées logiques et coordonnées du monde pour les tiles et les acteurs
+--  * Le imageOffset semble ne bien fonctionner que quand le ZoomLeval est à 1 (cf affichage des pieds jaunes si zommlevel <> 1)
 
 -- **EVOLS**
 --  * Centrer l'affichage sur le joueur si appui sur la molette
